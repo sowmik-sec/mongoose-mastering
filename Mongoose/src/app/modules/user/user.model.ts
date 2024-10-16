1eb8ee5d2cfafa5ffa1b76438d544e6aaae6a0c7
sowmik-sec <sowmik.sec@gmail.com>
2024-10-16 21:10:44 +0600

    create user model

---
+++ Mongoose/src/app/modules/user/user.model.ts
@@ -0,0 +1,61 @@
+import { model, Schema } from "mongoose";
+import { IUser } from "./user.interface";
+
+export const userSchema = new Schema<IUser>({
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+
+  name: {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    middleName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+  },
+  dateOfBirth: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female"],
+    required: true,
+  },
+
+  email: { type: String, required: true, unique: true },
+  contactNo: {
+    type: String,
+    required: true,
+  },
+  emergencyContactNo: {
+    type: String,
+    required: true,
+  },
+  presentAddress: {
+    type: String,
+    required: true,
+  },
+  permanentAddress: {
+    type: String,
+    required: true,
+  },
+});
+export const User = model<IUser>("User", userSchema);
