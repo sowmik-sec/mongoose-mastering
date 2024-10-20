9d6dcfa2d239b33085103633dc4f6fced2c27342
sowmik-sec <sowmik.sec@gmail.com>
2024-10-20 20:23:06 +0600

    create book model

---
+++ assignment-2/src/modules/book/book.model.ts
@@ -0,0 +1,56 @@
+import mongoose, { Schema } from "mongoose";
+import { IBook } from "./book.interface";
+
+export const bookSchema = new Schema<IBook>({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  author: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  genre: {
+    type: String,
+    required: true,
+  },
+  publication_year: {
+    type: Number,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  publisher: {
+    name: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+  },
+  reviews: [
+    {
+      user: {
+        type: String,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+export const Book = mongoose.model<IBook>("Book", bookSchema);
